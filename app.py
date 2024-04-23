796aa724b0c81ae2daa6e82530731c192a1c945b
ganeshpandey16 <138892958+ganeshpandey16@users.noreply.github.com>
2024-04-23 09:04:21 +0000

    add jobs and api to the websiyte

--- app.py
+++ app.py
@@ -1,8 +1,47 @@
-from flask import Flask
+from flask import Flask,render_template,jsonify
 app = Flask(__name__)
+
+JOBS=[
+  {
+    'id':1,
+    'title':'data analyst',
+    'location':'hyderabad',
+    'salary': '2000'
+
+  },
+  {
+    'id':2,
+    'title':'data analyst',
+    'location':'hyderabad',
+    'salary': '2000'
+
+  },
+  {
+    'id':3,
+    'title':'data analyst',
+    'location':'hyderabad',
+    'salary': '2000'
+
+  },
+  {
+    'id':4,
+    'title':'data analyst',
+    'location':'hyderabad',
+    'salary': '2000'
+
+  },
+  
+
+
+]
+
 @app.route("/")
 def hello():
-  return "hello world"
+  return render_template('home.html',jobs=JOBS)
 
+@app.route('/api/jobs')
+def api():
+  return jsonify(JOBS)
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
